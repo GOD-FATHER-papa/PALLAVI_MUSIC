d5c84e0479d6608f781c00bf6e62f81a90004b1c
𓂃❛ ⟶̽ 𝗦𝗰𝘆 ✘ 𝗢𝗽 ❜ 🌙⤹🌸 <imrealsanatani@gmail.com>
2026-03-27 22:19:22 +0530

    Initial commit

--- AnonMusic/__init__.py
+++ AnonMusic/__init__.py
@@ -1,10 +1,22 @@
+from .logging import LOGGER
+
 from AnonMusic.core.bot import Anony
+from AnonMusic.core.userbot import Userbot
+from AnonMusic.utils.downloader import Downloader
+
 from AnonMusic.core.dir import dirr
 from AnonMusic.core.git import git
-from AnonMusic.core.userbot import Userbot
 from AnonMusic.misc import dbb, heroku
 
-from .logging import LOGGER
+from .platforms import (
+    AppleAPI,
+    CarbonAPI,
+    SoundAPI,
+    SpotifyAPI,
+    RessoAPI,
+    TeleAPI,
+    YouTubeAPI,
+)
 
 dirr()
 git()
@@ -13,8 +25,7 @@ heroku()
 
 app = Anony()
 userbot = Userbot()
-
-from .platforms import *
+downloader = Downloader()
 
 Apple = AppleAPI()
 Carbon = CarbonAPI()
