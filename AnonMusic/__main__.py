a247f2fcc8eaa577e4d52249ef6d50478d3f6505
𓂃❛ ⟶̽ 𝗦𝗰𝘆 ✘ 𝗢𝗽 ❜ 🌙⤹🌸 <imrealsanatani@gmail.com>
2026-03-27 17:18:15 +0530

    Initial commit

--- AnonMusic/__main__.py
+++ AnonMusic/__main__.py
@@ -10,8 +10,8 @@ from AnonMusic.core.call import Anony
 from AnonMusic.misc import sudo
 from AnonMusic.plugins import ALL_MODULES
 from AnonMusic.utils.database import get_banned_users, get_gbanned
-from config import BANNED_USERS, COOKIES_URL
-from AnonMusic.plugins.sudo.cookies import set_cookies
+from config import BANNED_USERS
+
 
 async def init():
     if (
@@ -21,7 +21,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("⚙️ Assistant client variables not defined, exiting...")
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
     await sudo()
     try:
@@ -36,14 +36,14 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("AnonMusic.plugins" + all_module)
-    LOGGER("AnonMusic.plugins").info("✅ Successfully Imported Modules.")
+    LOGGER("AnonMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Anony.start()
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AnonMusic").error(
-            "🚫 Please turn on the videochat of your log group\channel. Stopping Bot..."
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
@@ -52,13 +52,10 @@ async def init():
     LOGGER("AnonMusic").info(
         "\x41\x6e\x6f\x6e\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x56\x69\x62\x65\x42\x6f\x74\x73\x20\x3a\x20\x47\x65\x74\x20\x46\x72\x65\x65\x20\x59\x6f\x75\x54\x75\x62\x65\x20\x41\x70\x69\x20\x3a\x20\x40\x4b\x65\x79\x4d\x61\x6b\x65\x72\x52\x6f\x42\x6f\x74"
     )
-    
-    res = await set_cookies(COOKIES_URL)
-    LOGGER("AnonMusic").info(f"{res}")
-    await Anony.decorators()
     await idle()
     await app.stop()
-    LOGGER("AnonMusic").info("🚫 Stopping AnonMusic Bot...")
+    await userbot.stop()
+    LOGGER("AnonMusic").info("Stopping Anony Music Bot...")
 
 
 if __name__ == "__main__":
