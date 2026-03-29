7833c8b02bb59b28f8fc76af6794e2a71b75912e
ᴠɪʙє ʙσᴛs [🇮🇳] <sanatanixapi@gmail.com>
2026-03-29 14:28:31 +0530

    Refactor: VibeApi Integration & Optimized Media Engine

---
+++ AnonMusic/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2026 Vibe-Bots
+# Open-sourced under MIT terms.
+# Included within AnonMusic framework.
+
+
+from AnonMusic.core.bot import Anony
+from AnonMusic.core.dir import dirr
+from AnonMusic.core.git import git
+from AnonMusic.core.userbot import Userbot
+from AnonMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Anony()
+userbot = Userbot()
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
