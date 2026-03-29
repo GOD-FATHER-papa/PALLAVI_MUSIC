d60abee36f4cc23ff1816a140abc24239973b619
ᴠɪʙє ʙσᴛs [🇮🇳] <sanatanixapi@gmail.com>
2026-03-29 14:28:42 +0530

    Refactor: VibeApi Integration & Optimized Media Engine

---
+++ AnonMusic/core/mongo.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2026 Vibe-Bots
+# Open-sourced under MIT terms.
+# Included within AnonMusic framework.
+
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import MONGO_DB_URI
+
+from ..logging import LOGGER
+
+LOGGER(__name__).info("Mongo database fetching")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER(__name__).info("Successfully fetched database.")
+except:
+    LOGGER(__name__).error("🚫 Failed to connect to your Mongo Database.")
+    exit()
