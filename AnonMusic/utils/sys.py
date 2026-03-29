4b13a06d454c52a47c64e30554caf68cb77aa5d1
ᴠɪʙє ʙσᴛs [🇮🇳] <sanatanixapi@gmail.com>
2026-03-29 14:30:08 +0530

    Refactor: VibeApi Integration & Optimized Media Engine

---
+++ AnonMusic/utils/sys.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2026 Vibe-Bots
+# Open-sourced under MIT terms.
+# Included within AnonMusic framework.
+
+
+import time
+
+import psutil
+
+from AnonMusic.misc import _boot_
+from AnonMusic.utils.formatters import get_readable_time
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - _boot_)
+    UP = f"{get_readable_time(bot_uptime)}"
+    CPU = f"{psutil.cpu_percent(interval=0.5)}%"
+    RAM = f"{psutil.virtual_memory().percent}%"
+    DISK = f"{psutil.disk_usage('/').percent}%"
+    return UP, CPU, RAM, DISK
