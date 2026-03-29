956a8694e99385ea80857c2a68970ff222b62ded
ᴠɪʙє ʙσᴛs [🇮🇳] <sanatanixapi@gmail.com>
2026-03-29 14:30:25 +0530

    Refactor: VibeApi Integration & Optimized Media Engine

---
+++ AnonMusic/utils/inline/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2026 Vibe-Bots
+# Open-sourced under MIT terms.
+# Included within AnonMusic framework.
+
+
+from .extras import *
+from .help import *
+from .play import *
+from .queue import *
+from .settings import *
+from .speed import *
+from .start import *
