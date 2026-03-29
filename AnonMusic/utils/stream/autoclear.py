de7f322b2f8e5cd9d78e9750c64258d1def7a22d
ᴠɪʙє ʙσᴛs [🇮🇳] <sanatanixapi@gmail.com>
2026-03-29 14:30:39 +0530

    Refactor: VibeApi Integration & Optimized Media Engine

---
+++ AnonMusic/utils/stream/autoclear.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2026 Vibe-Bots
+# Open-sourced under MIT terms.
+# Included within AnonMusic framework.
+
+
+import os
+
+from config import autoclean
+
+
+async def auto_clean(popped):
+    try:
+        rem = popped["file"]
+        autoclean.remove(rem)
+        count = autoclean.count(rem)
+        if count == 0:
+            if "vid_" not in rem or "live_" not in rem or "index_" not in rem:
+                try:
+                    os.remove(rem)
+                except:
+                    pass
+    except:
+        pass
