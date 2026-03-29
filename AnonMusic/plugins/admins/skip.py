2e31b018d3788f8e04f1a97ab8aa3b27dfc023f7
𓂃❛ ⟶̽ 𝗦𝗰𝘆 ✘ 𝗢𝗽 ❜ 🌙⤹🌸 <imrealsanatani@gmail.com>
2026-03-29 13:12:49 +0530

    Initial commit

--- AnonMusic/plugins/admins/skip.py
+++ AnonMusic/plugins/admins/skip.py
@@ -2,6 +2,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 import config
+
 from AnonMusic import YouTube, app
 from AnonMusic.core.call import Anony
 from AnonMusic.misc import db
