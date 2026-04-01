9e7ea826d681e8190d112019ddbfd234d56eabc9
kirtibots <kritikumari8665@gmail.com>
2026-04-02 01:15:30 +0530

    Change MongoDB collection from Anon to Yukki

--- AnonMusic/core/mongo.py
+++ AnonMusic/core/mongo.py
@@ -12,7 +12,7 @@ from ..logging import LOGGER
 LOGGER(__name__).info("Mongo database fetching")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
+    mongodb = _mongo_async_.Yukki
     LOGGER(__name__).info("Successfully fetched database.")
 except:
     LOGGER(__name__).error("🚫 Failed to connect to your Mongo Database.")
